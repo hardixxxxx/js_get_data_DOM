9fd1da55faa0d10d510d96a33e555310d0658b67
Anton Polishchuk <hardixxxxx@gmail.com>
2025-02-28 11:29:53 +0100

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const populations = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+const tPopulation = calcPopulation(populations);
+const aPopulation = calcPopulation(populations, populations.length);
+
+totalPopulation.innerHTML = tPopulation.toLocaleString('en-US');
+averagePopulation.innerHTML = aPopulation.toLocaleString('en-US');
+
+function calcPopulation(population, average) {
+  const total = [...population].reduce((sum, p) => {
+    const n = +p.innerHTML.replaceAll(',', '');
+
+    return n + sum;
+  }, 0);
+
+  if (average) {
+    return Math.round(total / average);
+  }
+
+  return total;
+}
