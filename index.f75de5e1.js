f9e691b4e11b7101b0285dfceab6354998fa0092
Anton Polishchuk <hardixxxxx@gmail.com>
2025-02-28 11:30:57 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+'use strict';
+const populations = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const tPopulation = calcPopulation(populations);
+const aPopulation = calcPopulation(populations, populations.length);
+totalPopulation.innerHTML = tPopulation.toLocaleString('en-US');
+averagePopulation.innerHTML = aPopulation.toLocaleString('en-US');
+function calcPopulation(population, average) {
+    const total = [
+        ...population
+    ].reduce((sum, p)=>{
+        const n = +p.innerHTML.replaceAll(',', '');
+        return n + sum;
+    }, 0);
+    if (average) return Math.round(total / average);
+    return total;
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
